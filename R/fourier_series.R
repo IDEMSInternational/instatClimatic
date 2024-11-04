9c9f72cc33edf05f20f9362646e7b16b34ce0c78
lilyclements <lily@idems.international>
2024-11-04 13:57:37 +0000

    adding functions

---
+++ R/fourier_series.R
@@ -0,0 +1,20 @@
+#' Fourier Series
+#'
+#' @description This function calculates the Fourier series for a given input `x` with a specified number of terms `n` and period `period`.
+#'
+#' @param x The input value for which the Fourier series is calculated.
+#' @param n The number of terms in the Fourier series.
+#' @param period The period of the Fourier series.
+#'
+#' @return The Fourier series expression as a character string.
+#' @export
+#'
+#' @examples
+#' fourier_series(3, 5, 2)
+fourier_series <- function(x, n, period) {
+  p2 <- "2 * pi"
+  h <-  seq_len(n)
+  paste0("sin(", x, " * ", h, " * ", p2, " / ", period, ")", " + ", 
+         "cos(", x, " * ", h, " * ", p2, " / ", period, ")", 
+         collapse = " + ")
+}
---
+++ R/global.R
@@ -0,0 +1,6 @@
+utils::globalVariables(c("Element", "Value", "value", "element.na", "From", "Date.ym", "na", "is.complete", "To", "Date.y", "Level", "count", "no",
+                         ".data", "month_name_english","month_abb_english",
+                         "fq","labels_label","data_unstacked",".","level.out",
+                         "index.search", "data_unstacked", ".",
+                         "cum", "cum1", "x", "y",
+                         "countriesHigh","countriesLow"))
